5b11a099965914d50f16a4fd0efbd46306606ee8
Eoin O'Brien <eoobrien@microsoft.com>
2024-07-31 12:13:26 +0100

    don't publish future posts

--- blog/src/lib/post-api.ts
+++ blog/src/lib/post-api.ts
@@ -12,7 +12,7 @@ export function getPostSlugs() {
   return fs.readdirSync(postsDirectory);
 }
 
-export function getPostBySlug(slug: string) {
+export function getPostBySlug(slug: string): Post {
   const realSlug = slug.replace(/\.md$/, "");
   const fullPath = join(postsDirectory, `${realSlug}.md`);
   const fileContents = fs.readFileSync(fullPath, "utf8");
@@ -31,6 +31,9 @@ export function getAllPosts(): Post[] {
   const slugs = getPostSlugs();
   const posts = slugs
     .map((slug) => getPostBySlug(slug))
+    // filter out posts whose publication date is after now,
+    // This only happens at build time.
+    .filter(post => post.date <= new Date().toISOString())
     // sort posts by date in descending order
     .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
 
