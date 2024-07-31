0585896d79ed54351dbd4c3b2cba97a2733cfbd0
Eoin O'Brien <eoobrien@microsoft.com>
2024-07-31 11:40:11 +0100

    center status icon and text

--- blog/src/app/components/status.tsx
+++ blog/src/app/components/status.tsx
@@ -7,7 +7,7 @@ export function Status({ enabled, children }: Props) {
   return (
     <span
       className={
-        "body-font align-middle rounded-md px-2 py-1 border-2 text-sm " +
+        "body-font rounded-md px-2 py-1 border-2 text-xs inline-flex items-center " +
         (enabled
           ? "bg-green-100 border-green-300 dark:bg-green-950 dark:border-green-900"
           : "bg-zinc-200 border-zinc-300 dark:bg-zinc-800 dark:border-zinc-700")
@@ -15,11 +15,11 @@ export function Status({ enabled, children }: Props) {
     >
       <span
         className={
-          "w-[0.7rem] h-[0.7rem] inline-block mr-2 rounded-full " +
+          "w-[0.7rem] h-[0.7rem] inline-block mr-1 rounded-full " +
           (enabled ? "bg-green-500" : "bg-zinc-400")
         }
       ></span>
-      <span className="uppercase">{children}</span>
+      {children}
     </span>
   );
 }
